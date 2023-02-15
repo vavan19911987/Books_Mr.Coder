9ba327e3e9d2db693d54cdf7efa1177d04965bfc
vavan19911987 <vavan7520@gmail.com>
2023-02-15 19:50:44 +0500

    прохожу гÑлаыву обьекты

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,151 +1,65 @@
 "use strict";
-// let a = 10;
-// let b = 2;
-// let c = a + b;
-// let d = a - b;
-// let f = a * b;
-// let g = a / b;
-// console.log(c + ' ' + 'сумма');
-// console.log(d + ' ' + 'разность');
-// console.log(f + ' ' + 'произведение');
-// console.log(g + ' ' + 'частное');
+// let obj = {
+//
+// }
+//
+// obj['a'] = 1;
+// obj['b'] = 2;
+// obj['c'] = 3;
+//
+// console.log(obj);
+
+// let obj = {
+//     22: 'a',
+//     33: 'b',
+//     12: 'c'
+// }
+//
+// console.log(obj);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let keys2 = Object.keys(obj);
+// let keys = Object.keys(obj).length;
+// let keys3 = 'x';
+// console.log(keys2);
+// console.log(keys);
+// console.log(obj[keys3]);
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(obj['x']);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+// console.log(obj[key]);
+
+// let obj = {x: 1, y: 2, z: 3};
+//
+// let prop = 'x';
+// console.log(obj[prop]);
+
+// let obj = {x: 1, y: 2, prop: 3};
+//
+// let prop = 'x';
+// console.log(obj[prop]);
 
 
-// let a = 10;
-// let b = 5;
-// let c = a - b;
-// let d = 7;
-// let g = c + d;
-// let result = g;
-// console.log(result);
+// let key = 'x';
+//
+// let obj = {
+//     [key]: 1,
+//     y: 2,
+//     z: 3
+// };
+// console.log(obj);
 
-// let a = "Владимир";
-// let b = 10;
-// console.log(a.length);
-
-// let a = "10px";
-// let b = "10px";
-// let x = parseFloat(a) + parseFloat(b);
-// console.log(x + "px");
-
-
-// let a = 123456789;
-// let b = 123456789;
-// console.log(String(a).length + String(b).length);
-
-// let str = "abcdf";
-// let str2 = str[4] + str[3] + str[2] + str[1] + str[0];
-// console.log(str, str2);
-
-// let str = 'abcde';
-// let num = 4;
-// console.log(str[num]);
-
-// let str = 'abcde';
-// let str2 = str.length - 3;
-// console.log(str[str2]);
-
-// let str = '1234';
-// let sum = Number(str[0]) + Number(str[1]);
-// console.log(sum);
-
-// let num = 12345;
-// let str = String(num);
-// let sum = Number(str[0]) + Number(str[1]);
-// console.log(sum);
-
-
-// let num = 12345;
-// let str = String(num);
-// let sum = Number(str[1]) * Number(str[2]);
-// let sum2 = str[1] * str[2];
-// console.log(sum, sum2);
-
-
-// let num = 12345;
-// let str = String(num);
-// let leng = str.length;
-// let str2 = str[4] + str[3] + str[2] + str[1] + str[0];
-// console.log(leng + ' длинна строки', str + ' обычная строка', str2 + ' строка в обратно порядке');
-
-
-// let num = 47;
-// num += 7;
-// num -= 18;
-// num *= 10;
-// num /= 15;
-// alert(num);
-
-// let num = 10;
-// num++;
-// num++;
-// num--;
-// alert(num);
-
-// let num1 = 3;
-// num1++;
-// console.log(num1);//4
-// let num2 = num1--;
-// console.log(num2, num1); //4 3
-// console.log(num1++); //3
-// console.log(--num2);//3
-
-// let a = 0.8;
-// let b = 0.2;
-// let c = a - b;
-// console.log(+c.toFixed(2));
-
-
-// let num1 = +prompt('Введите первое число');
-// let num2 = +prompt('Введите второе число');
-// console.log(num1 + num2);
-
-// let a = +prompt('Введите ширину прямоугольника');
-// let b = +prompt('Введите длинну прямоугольника');
-// const n = 2;
-// let p = (a + b) * n;
-// console.log(p);
-
-// let num1 = 1;
-// let num2 = 2;
-// console.log('сумма: ', num1 + num2);
-
-// let a = 1;
-// let b = 2;
-// let c = 3;
-// console.log(a + b + c);
-
-// let num = '123';
-// let sum = +num[0] + +num[1]
-//  + +num[2];
-// console.log(sum);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str[0]);
-
-// let a = 0;
-// console.log(++a);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str.length);
-
-// let a = 24 * 60 * 60;
-// console.log(a);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str.length -0);
-
-// let a = '123';
-// let b = '456';
-// let s = Number(a) + Number(b);
-
-// console.log(s);
-
-// let aaa = 1;
-// let bbb = 2;
-// let ccc = 3;
-
-// console.log(aaa + bbb + ccc);
+// let key1 = 'x';
+// let key2 = 'y';
+// let key3 = 'z';
+//
+// let obj = {
+//     [key1]: 1,
+//     [key2]: 2,
+//     [key3]: 3
+// };
+//
+// console.log(obj);
