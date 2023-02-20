0b4c614972d0943f12cfd007037e20ff6baa0916
Vavan19911987 <vavan7520@gmail.com>
2023-02-20 20:13:36 +0500

    продалжаю изучать if else

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,144 +1,133 @@
 "use strict";
-// let test = -1;
-// let test2 = 4;
+// let day = 30;
+//
+// if (day <= 10) {
+//     console.log('перва декада месяца');
+// } else if (day <= 20) {
+//     console.log('вторая декада месяца');
+// } else if (day <= 31) {
+//     console.log('третья декада месяца')
+// } else {
+//     console.log('no');
+// }
 
-// if (test <= 1 && test2 >= 3) {
-//  console.log(true)
+// let num = 99;
+//
+// if (num >= 10) {
+//     if (num <= 99) {
+//        num = String(num);
+//         let sum = +num[0] + +num[1]
+//         console.log(sum);
+//         if (sum <= 9) {
+//             console.log('однозначное число');
+//         }
+//         if (sum >= 10) {
+//             console.log('двухзначное число');
+//         }
+//     }
 // }else {
-//  console.log(false)
+//     console.log('введите двух значное число');
 // }
 
 
-// let num = 3;
+// let lang = 'ru';
+// switch (lang) {
+//     case 'en':
+//         console.log('en');
+//         break;
+//     case 'de':
+//         console.log('de');
+//         break;
+//     case 'ru':
+//         console.log('ru');
+//         break;
+//     default:
+//         console.log('no');
+// }
 
-// if (num > 0 && num < 5 || num > 10 && num
-//  < 20) {
-//  console.log('+++');
+
+
+// let num = -2;
+// let res = num >= 0 ? '1' : '2';
+//
+// console.log(res);
+
+// let a = 2 * (3 - 1);
+// let b = 6 - 2;
+// let result = a == b;
+// console.log(result);
+
+// let a = 5 * (7 - 4);
+// let b = 1 + 2 + 7;
+//
+// console.log(a > b);
+
+// let a = 2 ** 4;
+// let b = 4 ** 2;
+// console.log(a != b)
+
+
+// let agg = confirm('Вам есть 18 лет');
+//
+// if (agg) {
+//     alert('вы взрослый');
+// }else {
+//     alert('вы еще юнны');
+// }
+
+// let age = 18;
+// let adult;
+//
+// if (age >= 18) {
+//     adult = true;
 // } else {
-//  console.log('---');
+//     adult = false;
 // }
+// console.log(adult);
 
-// let num = 3;
 
-// if (num == 0 || num > 1 && num < 5 ) {
-//  console.log('+++');
+// let age = 22;
+// let res;
+//
+// if (age >= 18) {
+//     if (age <= 23) {
+//         res = 'от 18 до 23';
+//     } else {
+//         res = 'больше 23';
+//     }
 // } else {
-//  console.log('---');
+//     res = 'меньше 18';
 // }
+//
+// console.log(res);
 
-// let num = 3;
-
-// if ( (num > 0 && num < 5) || (num > 10 && num
-//  < 20) ) {
-//  console.log('+++');
+// let age = 26;
+// let res;
+//
+// if (age >= 18) {
+//     if (age <= 23) {
+//         res = 'от 18 до 23';
+//     } else {
+//         res = 'больше 23';
+//     }
 // } else {
-//  console.log('---');
+//     res = 'меньше 18';
 // }
+//
+// console.log(res);
 
-// let num1 = 3;
-// let num2 = 5;
-
-// if ( num1 > 0 && (num2 == 3 || num2 == 5) ) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
+// let min = 27;
+//
+// if (min >= 0 && min <= 19) {
+//     console.log('1 треть');
 // }
-
-
-// let num = 3;
-
-// if (num > (5 && num < 10) || num == 20) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
+//
+// if (min >= 20 && min <= 39) {
+//     console.log('2 треть');
 // }
-
-// let num = 3;
-
-// if (num > 5 || num > (0 && num < 3)) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-// let num = 3;
-
-// if (num == 9  num > (10 && num < 20)  num
-//  > 20 && num < 30) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
+//
+// if (min >= 40 && min <= 60) {
+//     console.log('3 треть');
 // }
 
 
-
-// if (num <= 0 || num >= 5) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-// if ( !(num > 0 && num < 5) ) {
-//  .log('+++');
-// } else {
-//  console.log('---');
-// }
-
-// let num1 = 5;
-// let num2 = 7;
-
-// if (!(num1 >= 0 || num2 <= 10)) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-
-// let test;
-
-// if (test) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-
-// let test1 = true;
-// let test2 = true;
-
-// if (!test1 && !test2) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-
-// let test1 = true;
-// let test2 = true;
-// let test3 = true;
-
-// if (test1 && test2  && test3) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-// let test1 = true;
-// let test2 = true;
-// let test3 = true;
-
-// if (test1 || test2 && test3) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
-
-// let test1 = true;
-// let test2 = true;
-// let test3 = true;
-
-// if (test1 || !test2  && !test3) {
-//  console.log('+++');
-// } else {
-//  console.log('---');
-// }
