087f88c29813c48963359d93f42e8b5cbf2e5058
Vavan19911987 <vavan7520@gmail.com>
2023-06-28 21:05:46 +0500

    ПОВТОР IIFE Immediately Invoked Function Expression НОВАЯ ТЕМА В УЧЕБНИКЕ

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,107 +1,76 @@
 'use strict'
 
-// ПОВТОР Замыкания //
+// ПОВТОР IIFE Immediately Invoked Function Expression НОВАЯ ТЕМА В УЧЕБНИКЕ //
 
-// Самостоятельно, не подсматривая в мой код, реализуйте счетчик вызова функции, работающий на замыканиях.
+// Допишите следующий код так, чтобы его запуск алертом выводил '!':
 
-// function test(num) {
+// let func = (function() {
 // 	return function () {
-// 		++num
-// 		return num;
-// 	}
-// }
-//
-// let func1 = test(0);
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-
-// Пусть функция в замыкании хранит число 10. Сделайте так,
-// чтобы каждый вызов функции уменьшал это число на 1 и выводил в консоль уменьшенное число.
-
-// function test(num) {
-// 	return function () {
-// 		--num
-// 		return num;
-// 	}
-// }
-//
-// let func1 = test(10);
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-// console.log(func1())
-
-
-// Модифицируйте предыдущую задачу так, чтобы отсчет доходил до 0, а
-// затем каждый последующий вызов функции выводил в консоль сообщение о том, что отсчет окончен.
-
-// function test(num) {
-// 	return function() {
-// 		if (num >= 0) {
-// 			console.log(num);
-// 			num--;
-// 		} else {
-// 			console.log('отсчет окончен');
+// 		return function () {
+// 			return '!';
 // 		}
+// 	}
+// })()()();
+// console.log(func);
+
+// Допишите следующий код так, чтобы его запуск выводил сумму переданных параметрами чисел:
+
+// let func = (function (num1) {
+// 	return function (num2) {
+// 		return num1 + num2;
+// 	}
+// })(1)(2)
+// console.log(func);
+
+// Допишите следующий код так, чтобы его запуск выводил сумму переданных параметрами чисел:
+
+// let func = (function (num1) {
+// 	return function (num2) {
+// 		return function (num3) {
+// 			return num1 + num2 + num3;
+// 		}
+// 	}
+// })(1)(2)(3)
 //
-// 	};
-// }
+// console.log(func);
+
+// Не подсматривая в мой код, самостоятельно реализуйте такой же счетчик.
+
+// let func = (function () {
+// 	let num = 1;
+// 	return function () {
+// 		console.log(num)
+// 		num++
+// 	}
+// })();
 //
-// let func1 = test(10);
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
+// func()
+// func()
+// func()
+// func()
 
-function test() {
-	let counter = 0;
+// Модифицируйте работу счетчика так, чтобы он считал до 5, а потом отсчет начинался сначала.
 
-	return function() {
-		return function() {
-			console.log(counter);
-			counter++;
-		};
-	};
-}
-
-let func = test();
-
-let func1 = func();
-let func2 = func();
-func1();
-func2();
-func1();
-func2();
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let func = (function () {
+// 	let num = 1;
+// 	return function () {
+// 		if (num > 5) {
+// 			num = 1
+// 		}
+// 		console.log(num)
+// 		num++
+// 	}
+// })();
+//
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
 
