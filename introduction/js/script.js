31ca4545e1dba2311ed18f165b54ee7123a6d042
Vavan19911987 <vavan7520@gmail.com>
2023-07-12 20:22:38 +0500

    ПОВТОР Таймеры

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,164 +1,310 @@
 'use strict'
 
-// ПОВТОР Контекст//
+// ПОВТОР Таймеры//
 
-// let elem = document.querySelector('#elem');
-// elem.addEventListener('blur', parent);
-//
-// function parent() {
-// 	console.log(this); // выведет
-// 	//ссылку на наш инпут
-//
-// 	function child() {
-// 		console.log(this); // выведет
-// 		//undefined
+// Запустите таймер, который каждые 3 секунды будет что-нибудь выводить в консоль.
+// setInterval(function() {
+// 	console.log('1');
+// }, 3000);
+
+// Пусть дана переменная, в которой изначально хранится число 100.
+// Запустите таймер, который каждую секунду будет уменьшать значение
+// этой переменной на 1 и выводить это значение в консоль.
+
+// let i = 100;
+// setInterval(function () {
+// 	while (i >= 0) {
+// 		console.log(i--)
 // 	}
-// 	child();
+// },1000)
+
+// Пусть дана переменная, в которой изначально
+// хранится число 10. Запустите таймер, который каждую
+// секунду будет уменьшать значение этой переменной на 1 и выводить
+// это значение в консоль. Как только значение переменной достигнет нуля
+// - остановите таймер.
+// let i = 100;
+// let timeId = setInterval(function () {
+// 	console.log(i--)
+// 	if (i <= 0) {
+// 		clearInterval(timeId)
+// 	}
+// },500)
+
+// Сделайте кнопку, по нажатию на которую в консол
+// ь будет выводится обратный отсчет, начиная с 100.
+// <input type="submit" name="Start" id="start">
+// <p class="par"></p>
+
+// let start = document.querySelector('#start');
+// let par = document.querySelector('.par');
+//
+// start.addEventListener('click', function starts() {
+// 	let testTime = 100;
+// 	let test = setInterval(() => {
+// 		par.innerHTML = String(testTime--);
+// 		if (testTime <= 0) {
+// 			clearInterval(test)
+// 		}
+// 	},200)
+// 	this.removeEventListener('click', starts);
+// })
+
+// let start = document.querySelector('#start')
+// let stop = document.querySelector('#stop');
+// let timerId;
+// start.addEventListener('click', function () {
+// 	let i = 0
+//
+// 	timerId = setInterval(function () {
+// 		console.log('!')
+// 	}, 1000)
+// })
+// // Останавливаем таймер:
+// stop.addEventListener('click', function () {
+// 	clearInterval(timerId)
+// })
+
+// Пусть дана переменная, в которой изначально хранится число 100.
+// Даны также две кнопки. По нажатию на первую кнопку запустите таймер,
+// который каждую секунду будет уменьшать значение переменной на 1 и выводить
+// новое значение в консоль. Как только значение переменной достигнет нуля -
+// остановите таймер.
+// По нажатию на вторую кнопку остановите таймер.
+// Также остановите таймер, если вторая кнопка не была нажата,
+// но значение переменной достигло нуля.
+//<button id="start">start</button>
+// <button id="stop">stop</button>
+// <p class="par"></p>
+
+// let setTime
+// let num = 100
+//
+// let start = document.querySelector('#start')
+// start.addEventListener('click', starts)
+//
+// function starts () {
+// 	setTime = setInterval(() => {
+// 		let par = document.querySelector('.par')
+// 		par.innerHTML = String(num--)
+// 		if (num <= -1) {
+// 			clearInterval(setTime)
+// 		}
+// 	}, 500)
+// 	this.removeEventListener('click', starts)
 // }
+//
+// let stop = document.querySelector('#stop')
+// stop.addEventListener('click', () => {
+// 	clearInterval(setTime)
+// 	start.addEventListener('click', starts)
+// })
+
+// Некоторый программист написал код, который по нажатию на кнопку запускает таймер, выводящий в консоль текущий момент времени:
+
+// <input type="submit" id="start"
+// 	   value="start">
+// 	<input type="submit" id="stop"
+// 		   value="stop">
+// <!--<p class="par"></p>-->
+// let start = document.querySelector('#start')
+// let stop = document.querySelector('#stop')
+// let par = document.querySelector('.par')
+// let timerId
+// start.addEventListener('click', function () {
+// 	timerId = setInterval(function () {
+// 		let date = new Date
+// 		par.innerHTML = date.getHours() + ':' + date.getMinutes() + ':' +
+// 			date.getSeconds()
+// 	}, 1000)
+// })
+//
+// stop.addEventListener('click', function () {
+// 	clearInterval(timerId)
+// })
+// После запуска кода, однако, оказалось, что кнопка
+// остановки не работает. Исправьте ошибку автора кода.
 
 // Пусть дан такой код:
-// 	<input id="elem" value="3">
-// 		"use strict";
+
+// <input type="button" id="elem" value="1">
+
+// Автор кода хотел, чтобы по нажатию на кнопку,
+// значение этой кнопки каждую секунду увеличивалось на 1.
+// Однако, по нажатию на кнопку вообще ничего не происходит.
+// Исправьте ошибку автора кода. Напишите текст, в котором вы дадите
+// объяснение автору кода, почему возникла его ошибка.
 
 // let elem = document.querySelector('#elem')
-// elem.addEventListener('blur', func)
 //
-// function func () {
+// elem.addEventListener('click', function () {
 // 	let self = this
-// 	function square () {
-// 		return self.value * self.value
-// 	}
-// 	console.log(square())
-// }
-
-// Автор кода хотел, чтобы по потери фокуса на экран
-// вывелся квадрат числа из value инпута.
-// Однако, почему-то при потери фокуса в консоль
-// выдается ошибка. Исправьте ошибку автора кода.
-// Напишите текст, в котором вы дадите объяснение автору кода,
-// почему возникла его ошибка.
-
-// Возьмите код из предыдущей задачи и исправьте
-// проблему кода с помощью изученного второго способа.
-
-// let elem = document.querySelector('#elem')
-// elem.addEventListener('blur', func)
-//
-// function func () {
-// 	console.log(square(this))
-// 	function square (par) {
-// 		return par.value * par.value
-// 	}
-//
-// }
-
-// Возьмите код из предыдущей задачи
-//и исправьте проблему кода с помощью изученного третьего способа.
-
-// let elem = document.querySelector('#elem')
-// elem.addEventListener('blur', func)
-//
-// function func () {
-// 	let square = () =>  {
-// 		console.log(this.value * this.value)
-// 	}
-// 	square()
-// }
-
-// Дана функция:
-// Даны три инпута:
-
-// <input id="elem1" value="text1">
-// <input id="elem2" value="text2">
-// <input id="elem3" value="text3">
-// С помощью метода call и функции func
-// выведите на экран value каждого из инпутов.
-// let el1 = document.querySelector('#elem1')
-// let el2 = document.querySelector('#elem2')
-// let el3 = document.querySelector('#elem3')
-//
-// function func () {
-// 	console.log(this.value)
-// }
-//
-// func.call(el1)
-// func.call(el2)
-// func.call(el3)
-
-// Пусть дан следующий код:
-// <input id="elem" value="hello">
-
-//Добавьте в последнюю строчку метод call так,
-// чтобы на экран вывелось 'hello, John Smit'.
-// Слово 'hello' должно взяться из value инпута, а
-// 'John' и 'Smit' должны быть параметрами функциями.
-
-// let elem = document.querySelector('#elem');
-// elem.addEventListener('blur', () => {
-// 	func.call(elem,'Smit','John')
+// 	setInterval(function () {
+// 		self.value = Number(elem.value) + 1
+// 	}, 1000)
 // })
-// function func(surname, name) {
-// 	console.log(this.value + ', ' + name + ' ' + surname);
-// }
-// func.call(elem,'Smit','John');
 
-// Пусть дан следующий код:
-// <input id="elem" value="hello">
-
-//Добавьте в последнюю строчку метод call так,
-// чтобы на экран вывелось 'hello, John Smit'.
-// Слово 'hello' должно взяться из value инпута, а
-// 'John' и 'Smit' должны быть параметрами функциями.
-
-// let elem = document.querySelector('#elem');
-// let arr = ['Smit','John']
-// elem.addEventListener('blur', () => {
-// 	func.apply(elem,arr)
+// Дана кнопка. Дан абзац, текстом которого является число.
+// По нажатию на кнопку запустите таймер, который каждую
+// секунду будет увеличивать текст абзаца на 1.
+//<p class="par"></p>
+// <button id="start">start</button>
+// let par = document.querySelector('.par');
+// let btn = document.querySelector('#start');
+// btn.addEventListener('click', function () {
+// 	let i = 0;
+// 	let setInt = setInterval(() => {
+// 		par.innerHTML = String(i++)
+// 		if (i >= 11) {
+// 			clearInterval(setInt);
+// 		}
+// 	}, 1000)
 // })
-// function func(surname, name) {
-// 	console.log(this.value + ', ' + name + ' ' + surname);
+
+// Дана кнопка. Дан абзац, текстом которого является число,
+// например, 10. По нажатию на кнопку запустите таймер,
+// который каждую секунду будет уменьшать текст абзаца на 1.
+// Как только значение абзаца станет равно нулю - остановите таймер.
+//<p class="par"></p>
+// <button id="start">start</button>
+// let par = document.querySelector('.par');
+// let btn = document.querySelector('#start');
+// btn.addEventListener('click', function noClick() {
+// 	let i = 10;
+// 	let setInt = setInterval(() => {
+// 		par.innerHTML = String(i--)
+// 		if (i <= -1) {
+// 			clearInterval(setInt);
+// 		}
+// 	}, 1000)
+// 	this.removeEventListener('click', noClick);
+// })
+
+// Дан инпут с числом. Сделайте так, чтобы каждую секунду
+// в нем появлялся квадрат того числа, которое в нем записано.
+// <input type="text" id="test" value="2">
+// let inp = document.querySelector('#test');
+// inp.addEventListener('focus', function noFocus() {
+// 	let self = this
+// 	let setInt = setInterval(() => {
+// 		self.value = self.value ** 2;
+// 		if (self.value >= 1000) {
+// 			clearInterval(setInt);
+// 		}
+// 	}, 1000)
+// 	this.removeEventListener('focus', noFocus)
+// })
+
+// Дан инпут. В него вводится число. По потери фокуса сделайте
+// так, чтобы в абзаце ниже начал тикать обратный отсчет,
+// начиная с введенного числа. Когда отсчет дойдет до нуля -
+// он должен закончится.
+// <input type="text" id="test" value="2">
+// <p class="par"></p>
+// let inp = document.querySelector('#test')
+// let par = document.querySelector('.par')
+// inp.addEventListener('blur', stopFunc)
+// inp.addEventListener('focus', () => inp.value = '')
+//
+// function stopFunc (param) {
+// 	let self = this
+// 	let setInt = setInterval(() => {
+// 		par.innerHTML = String(self.value--)
+// 		if (self.value <= -1) {
+// 			clearInterval(setInt)
+// 		}
+// 	}, 1000)
+//
 // }
-// func.call(elem,'Smit','John');
 
-// Пусть дан следующий код:
-// 	<input id="elem" value="привет">
+// Дан инпут, кнопка и абзац. В инпут вводится какое-то число.
+// По нажатию на кнопку запишите введенное число в текст абзаца и
+// запустите обратный отсчет в абзаце: пусть каждую секунду число в
+// абзаце уменьшается на единицу, пока не дойдет до нуля.
+// <p class="par"></p>
+// <input type="text" id="test" value="Введите число">
+// <input type="button" id="elem" value="start">
 
-// Напишите в указанном месте конструкцию
-// с методом bind так, чтобы this внутри функции
-// func всегда указывал на инпут из переменной elem
-// let elem = document.getElementById('elem')
-//
-// function func (name, surname) {
-// 	console.log(this.value + ', ' + name + ' ' + surname)
+// let par = document.querySelector('.par')
+// let inp = document.querySelector('#test')
+// let btn = document.querySelector('#elem')
+// inp.addEventListener('focus', () => inp.value = '')
+// btn.addEventListener('click', function stopClick () {
+// 	par.innerHTML = inp.value
+// 	let setInt = setInterval(() => {
+// 		par.innerHTML--
+// 		if (par.innerHTML <= 0) {
+// 			clearInterval(setInt)
+// 		}
+// 	}, 1000)
+// 	setTimeout(() => {
+// 		inp.value = 'Введите число'
+// 	}, 1000)
+// })
+// Дан абзац и две кнопки. Сделайте так, чтобы по нажатию
+// на первую кнопку в абзаце начал тикать таймер от 1 до бесконечности,
+// а по нажатию на вторую таймер останавливался.
+// <p class="par"></p>
+// <button id="start">start</button>
+// <button id="stop">stop</button>
+
+// let par = document.querySelector('.par')
+// let btn1 = document.querySelector('#start')
+// let btn2 = document.querySelector('#stop')
+// let setInt;
+// let i = 0
+// btn1.addEventListener('click',stopFunc)
+// function stopFunc() {
+// 	setInt = setInterval(() => {
+// 		par.innerHTML = String(i++);
+// 	},1000)
+// 	this.removeEventListener('click', stopFunc);
 // }
+// btn2.addEventListener('click', () => {
+// 	clearInterval(setInt)
+// 	btn1.addEventListener('click', stopFunc);
+// });
+
+
+// Дан абзац. Сделайте так, чтобы каждую секунду
+// он менял свой цвет с красного на зеленый и наоборот.
+// <p class="par"></p>
+// let par = document.querySelector('.par');
+// let setInt = setInterval(() => {
+// 	par.classList.toggle('active')
+// }, 1000)
+
+// Если выводить на экран каждую секунду текущий
+// момент времени, то можно сделать тикающие часы.
+// Реализуйте такие же часики, как показано ниже в образце:
+// <p class="par"></p>
+// let par = document.querySelector('.par');
+// let setInt = setInterval(() => {
+// 	let date = new Date();
+// 	par.innerHTML = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+// })
+
+// Дан абзац. Напишите код, который выведет сообщение
+// в этот абзац через 10 секунд после загрузки страницы.
+// <p class="par"></p>
 //
-// // тут напишите конструкцию с bind()
+// let par = document.querySelector('.par');
+// let setTim = setTimeout(() => par.innerHTML = 'Hello men', 3000)
+
+// Выведите в консоль число 0.
+// Через секунду выведите число 1, через две секунды выведите число 2,
+// через 3 секунды выведите число 3. И так далее до бесконечности.
+
+// let i = 0;
 //
-// let newFunc = func.bind(elem) // тут должно вывести 'hello, John Smit'
-// let nowFunc = func.bind(elem) // тут должно вывести 'hello, Eric Luis'
-// newFunc('John', 'Smit')
-// nowFunc('Eric', 'Luis')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function timer() {
+// 	setTimeout(function() {
+// 		console.log(++i);
+//
+// 		timer(); // вызовем сами себя
+// 	}, 1000);
+// }
+// timer();
 
 
 
